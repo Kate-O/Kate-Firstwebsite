27838181ed6a82e4de965a68b091fe360b2df652
Kate-O <107926636+Kate-O@users.noreply.github.com>
2022-06-21 14:42:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+const pie=3.142 
+console.log(pie)
+// alert("You should read this book!!")
+// let num1 = 10
+// let num2 = 5
+// console.log(num1 + num2)
+
+
+function book1(num1,num2){
+    console.log(num1 / num2)
+}
+book1(5,1)
+
+function callBook(){
+    return "My name is Kate"
+}
+console.log(callBook())
